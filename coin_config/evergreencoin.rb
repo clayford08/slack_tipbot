1f237a2d396afa4cf084b745b5c3d7fcad9edcf7
clayford08 <clay_sonnier@yahoo.com>
2016-01-31 22:36:02 -0600

    Update evergreencoin.rb

--- coin_config/evergreencoin.rb
+++ coin_config/evergreencoin.rb
@@ -1,1 +1,22 @@
 
+module EverGreenCoin
+  PERFORM_ERROR = 'That command doesn\'t exist'
+  BALANCE_REPLY_PRETEXT = 'Your balance is'
+  CURRENCY_ICON = 'EGC'
+  WEALTHY_UPPER_BOUND = 50
+  WEALTHY_UPPER_BOUND_POSTTEXT = ' dude you\'re ballin!'
+  WEALTHY_UPPER_BOUND_EMOJI = ':moneybag:'
+  BALANCE_REPLY_POSTTEXT = ' Coins'
+  DEPOSIT_PRETEXT = 'Deposit EGC to this address: '
+  DEPOSIT_POSTTEXT = 'Deposited'
+  TIP_ERROR_TEXT = 'Error: tip @username amount'
+  TIP_PRETEXT = 'How nice of you.'
+  TIP_POSTTEXT1 = 'Tip Sent'
+  WITHDRAW_TEXT = 'Start Staking'
+  NETWORKINFO_ICON = ':bar_chart:'
+  TOO_POOR_TEXT = 'You don\'t have that much EGC!'
+  NO_PURPOSE_LOWER_BOUND_TEXT = 'Too low of an amount of EGC'
+  NO_PURPOSE_LOWER_BOUND = 1
+  RANDOMIZED_EMOJI = ':black_joker:'
+  NETWORK = 'evergreencoin'
+end
